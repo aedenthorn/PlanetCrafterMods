74c5c9f256a605dee7ed5e78365ca4dc36f175a4
aedenthorn <aedenthorn1@gmail.com>
2023-03-06 11:45:48 -0500

    etc

---
+++ AutoCrafterTweaks/BepInExPlugin.cs
@@ -0,0 +1,83 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using SpaceCraft;
+using System;
+using System.Reflection;
+
+namespace AutoCrafterTweaks
+{
+    [BepInPlugin("aedenthorn.AutoCrafterTweaks", "Auto Crafter Tweaks", "0.1.0")]
+    public partial class BepInExPlugin : BaseUnityPlugin
+    {
+        private static BepInExPlugin context;
+
+        public static ConfigEntry<bool> modEnabled;
+        public static ConfigEntry<bool> isDebug;
+
+        public static ConfigEntry<float> range;
+        public static ConfigEntry<float> interval;
+
+        public static void Dbgl(string str = "", LogLevel logLevel = LogLevel.Debug)
+        {
+            if (isDebug.Value)
+                context.Logger.Log(logLevel, str);
+        }
+        private void Awake()
+        {
+
+            context = this;
+            modEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable this mod");
+            isDebug = Config.Bind<bool>("General", "IsDebug", true, "Enable debug logs");
+            range = Config.Bind<float>("Options", "Range", 30f, "Range (m)");
+            interval = Config.Bind<float>("Options", "Interval", 1f, "Craft interval in seconds");
+
+            range.SettingChanged += SettingChanged;
+            interval.SettingChanged += SettingChanged;
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+            Dbgl("Plugin awake");
+            InvokeRepeating("CheckAutoHarvest", 0, interval.Value);
+
+            ResetValues();
+        }
+
+
+        private void SettingChanged(object sender, EventArgs e)
+        {
+            ResetValues();
+        }
+
+        private void ResetValues()
+        {
+            if (!modEnabled.Value)
+                return;
+            foreach(var m in FindObjectsOfType<MachineAutoCrafter>())
+            {
+                SetValues(m);
+            }
+        }
+
+        [HarmonyPatch(typeof(MachineAutoCrafter), "Start")]
+        private static class MachineAutoCrafter_Start_Patch
+        {
+            static void Prefix(MachineAutoCrafter __instance)
+            {
+                if (!modEnabled.Value)
+                    return;
+
+                SetValues(__instance);
+            }
+
+        }
+
+        private static void SetValues(MachineAutoCrafter machine)
+        {
+
+            machine.range = range.Value;
+            machine.craftEveryXSec = interval.Value;
+        }
+
+    }
+}
---
+++ CameraShakeTweaks/BepInExPlugin.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using SpaceCraft;
+using System.Reflection;
+
+namespace CameraShakeTweaks
+{
+    [BepInPlugin("aedenthorn.CameraShakeTweaks", "CameraShakeTweaks", "0.1.0")]
+    public partial class BepInExPlugin : BaseUnityPlugin
+    {
+        private static BepInExPlugin context;
+
+        public static ConfigEntry<bool> modEnabled;
+        public static ConfigEntry<bool> isDebug;
+        
+        public static ConfigEntry<bool> disableShake;
+        public static ConfigEntry<float> shakeMult;
+        public static ConfigEntry<float> decreaseMult;
+
+        public static void Dbgl(string str = "", LogLevel logLevel = LogLevel.Debug)
+        {
+            if (isDebug.Value)
+                context.Logger.Log(logLevel, str);
+        }
+        private void Awake()
+        {
+
+            context = this;
+            modEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable this mod");
+            isDebug = Config.Bind<bool>("General", "IsDebug", true, "Enable debug logs");
+            disableShake = Config.Bind<bool>("Options", "DisableShake", false, "Completely disable camera shake.");
+            shakeMult = Config.Bind<float>("Options", "ShakeMult", 0.5f, "Multiply shake effect by this amount.");
+            decreaseMult = Config.Bind<float>("Options", "DecreaseMult", 1f, "Multiply shake decrease over time by this amount.");
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+            Dbgl("Plugin awake");
+        }
+
+        [HarmonyPatch(typeof(PlayerCameraShake), nameof(PlayerCameraShake.SetShaking))]
+        private static class PlayerCameraShake_SetShaking_Patch
+        {
+            static void Prefix(PlayerCameraShake __instance, ref bool _isShaking, ref float _shakeValue, ref float _decreaseValue)
+            {
+                if (!modEnabled.Value)
+                    return;
+                if (disableShake.Value)
+                    _isShaking = false;
+                _shakeValue *= shakeMult.Value;
+                _decreaseValue *= decreaseMult.Value;
+
+            }
+        }
+    }
+}
