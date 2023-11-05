2fd6c21a192f7adbb4c64ba4fdad0563ad0cc453
Desperationfighter <patrick_as_desp@yahoo.de>
2023-11-05 21:31:39 +0100

    Improve Sorting algorythm to offer deeper settings.

--- QuickStore/BepInExPlugin.cs
+++ QuickStore/BepInExPlugin.cs
@@ -3,9 +3,11 @@ using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using SpaceCraft;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -23,6 +25,10 @@ namespace QuickStore
         private static ConfigEntry<string> allowList;
         private static ConfigEntry<string> disallowList;
         private static ConfigEntry<float> range;
+        private static ConfigEntry<bool> allowStoreIfContainerNameisexact;
+        private static ConfigEntry<bool> allowStoreIfContainerNameContains;
+        private static ConfigEntry<bool> requireNameFlagtoStore;
+        private static ConfigEntry<string> NameFlag;
 
         private static InputAction action;
 
@@ -43,6 +49,11 @@ namespace QuickStore
             allowList = Config.Bind<string>("Options", "AllowList", "", "Comma-separated list of item IDs to allow storing (overrides DisallowList).");
             disallowList = Config.Bind<string>("Options", "DisallowList", "", "Comma-separated list of item IDs to disallow storing (if AllowList is empty)");
             range = Config.Bind<float>("Options", "Range", 20f, "Store range (m)");
+            allowStoreIfContainerNameisexact = Config.Bind<bool>("Options", "allowStoreIfContainerNameisexact", true, "Allows to Store Item to an Container when it has the exact Name");
+            allowStoreIfContainerNameContains = Config.Bind<bool>("Options", "allowStoreIfContainerNameContains", true, "Allows to Store Item to an Container when it just Contains the Name of the Contant");
+            requireNameFlagtoStore = Config.Bind<bool>("Options", "requireNameFlagtoStore", false, "Requires a Nameflag beside the Object Name to Store stuff");
+            NameFlag = Config.Bind<string>("Sub-Options", "NameFlag", "AS", "(Only required if requireNameFlagtoStore set to true) One or More Characters, that stands free, also need to be in the Container Name to activate the Container for Storing. Example: \"AS Iron 3\" or \"Aluminium AS\" ");
+
 
             if (!storeKey.Value.Contains("<"))
                 storeKey.Value = "<Keyboard>/" + storeKey.Value;
@@ -105,7 +116,28 @@ namespace QuickStore
                         if (disallow.Contains(objects[j].GetGroup().GetId()))
                             continue;
                     }
-                    if (!inventory.IsFull() && (ial[i].GetComponent<WorldObjectText>()?.GetText() == Readable.GetGroupName(objects[j].GetGroup()) || inventory.GetInsideWorldObjects().Exists(o => o.GetGroup() == objects[j].GetGroup())))
+                    else if (inventory.IsFull()) continue;
+
+                    //Work with and Bool Flag to make to logic better to read.
+                    bool allownamedstore = false;
+                    //check if Storing, depeding on ContainerName is exactly the ObjectName, is allowed.
+                    if ( allowStoreIfContainerNameisexact.Value && ial[i].GetComponent<WorldObjectText>()?.GetText().ToLower() == Readable.GetGroupName(objects[j].GetGroup()).ToLower()) allownamedstore = true;
+                    //check if Storing, depeding on ContainerName contains ObjectName, is allowed. If yes check if Container has the Name to proceed
+                    if ( allowStoreIfContainerNameContains.Value && ( ial[i].GetComponent<WorldObjectText>().GetText().ToLower().Contains($" {Readable.GetGroupName(objects[j].GetGroup()).ToLower()} ") || ial[i].GetComponent<WorldObjectText>().GetText().ToLower().StartsWith($"{Readable.GetGroupName(objects[j].GetGroup()).ToLower()} ") || ial[i].GetComponent<WorldObjectText>().GetText().ToLower().EndsWith($" {Readable.GetGroupName(objects[j].GetGroup()).ToLower()}")) )
+                    {
+                            //Now check if a NameFlag is required
+                            if (requireNameFlagtoStore.Value)
+                            {
+                                //Check for the Name Flag. To make sure to not make a false positiv only accept free standing Flags. Include Start and End where a Space is missing.
+                                if ( ial[i].GetComponent<WorldObjectText>().GetText().ToLower().Contains($" {NameFlag.Value.ToLower()} ") || ial[i].GetComponent<WorldObjectText>().GetText().ToLower().StartsWith($"{NameFlag.Value.ToLower()} ") || ial[i].GetComponent<WorldObjectText>().GetText().ToLower().EndsWith($" {NameFlag.Value.ToLower()}")) allownamedstore = true;
+                            }
+                            else
+                            {
+                                allownamedstore = true;
+                            }
+                    }
+
+                    if (allownamedstore || inventory.GetInsideWorldObjects().Exists(o => o.GetGroup() == objects[j].GetGroup()) )
                     {
                         Dbgl($"Storing {objects[j].GetGroup()} in {ial[i].name}");
                         if (inventory.AddItem(objects[j]))
