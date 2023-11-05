bf7a6cafee09a069a386dbb64d98e6e3a1f91c52
aedenthorn <aedenthorn1@gmail.com>
2023-11-05 18:01:48 -0500

    etc

--- QuickStore/BepInExPlugin.cs
+++ QuickStore/BepInExPlugin.cs
@@ -23,11 +23,10 @@ namespace QuickStore
         private static ConfigEntry<string> allowList;
         private static ConfigEntry<string> disallowList;
         private static ConfigEntry<float> range;
-        private static ConfigEntry<bool> allowstoreIfContainerAlreadyContainsSameItem;
-        private static ConfigEntry<bool> allowStoreIfContainerNameisexact;
-        private static ConfigEntry<bool> allowStoreIfContainerNameContains;
-        private static ConfigEntry<bool> requireNameFlagtoStore;
-        private static ConfigEntry<string> NameFlag;
+        private static ConfigEntry<bool> storeIfAlreadyContains;
+        private static ConfigEntry<bool> storeIfContainerNameExact;
+        private static ConfigEntry<bool> storeIfContainerNameContains;
+        private static ConfigEntry<string> requireNameFlagtoStore;
 
         private static InputAction action;
 
@@ -48,11 +47,10 @@ namespace QuickStore
             allowList = Config.Bind<string>("Options", "AllowList", "", "Comma-separated list of item IDs to allow storing (overrides DisallowList).");
             disallowList = Config.Bind<string>("Options", "DisallowList", "", "Comma-separated list of item IDs to disallow storing (if AllowList is empty)");
             range = Config.Bind<float>("Options", "Range", 20f, "Store range (m)");
-            allowstoreIfContainerAlreadyContainsSameItem = Config.Bind<bool>("Options", "allowstoreIfContainerAlreadyContainsSameItem", true, "Allows to Store Item to an Container when it already have the Same Item Type in it.");
-            allowStoreIfContainerNameisexact = Config.Bind<bool>("Options", "allowStoreIfContainerNameisexact", true, "Allows to Store Item to an Container when it has the exact Name");
-            allowStoreIfContainerNameContains = Config.Bind<bool>("Options", "allowStoreIfContainerNameContains", true, "Allows to Store Item to an Container when it just Contains the Name of the Contant");
-            requireNameFlagtoStore = Config.Bind<bool>("Options", "requireNameFlagtoStore", false, "Requires a Nameflag beside the Object Name to Store stuff");
-            NameFlag = Config.Bind<string>("Sub-Options", "NameFlag", "AS", "(Only required if requireNameFlagtoStore set to true) One or More Characters, that stands free, also need to be in the Container Name to activate the Container for Storing. Example: \"AS Iron 3\" or \"Aluminium AS\" ");
+            storeIfAlreadyContains = Config.Bind<bool>("Options", "StoreIfAlreadyContains", true, "Store item in an container when it already has the same item type in it.");
+            storeIfContainerNameExact = Config.Bind<bool>("Options", "StoreIfContainerNameExact", true, "Store item in an container when the container has the exact name of the item.");
+            storeIfContainerNameContains = Config.Bind<bool>("Options", "StoreIfContainerNameContains", true, "Store item in an container when the container name contains the name of the item.");
+            requireNameFlagtoStore = Config.Bind<string>("Options", "requireNameFlagtoStore", "", "Require this tag in the container name to store item");
 
 
             if (!storeKey.Value.Contains("<"))
@@ -104,8 +102,15 @@ namespace QuickStore
 
                 Dbgl($"checking close inventory {ial[i].name}: {ial[i].transform.position}, {pos}: {dist}m");
 
+                if (!string.IsNullOrEmpty(requireNameFlagtoStore.Value) && !ial[i].GetComponent<WorldObjectText>().GetText().ToLower().Contains($"{requireNameFlagtoStore.Value}"))
+                    continue;
+
+                var containerName = ial[i].GetComponent<WorldObjectText>()?.GetText().ToLower();
+
                 for (int j = objects.Count - 1; j >= 0; j--)
                 {
+                    if (inventory.IsFull())
+                        break;
                     if (allowList.Value.Length > 0)
                     {
                         if (!allow.Contains(objects[j].GetGroup().GetId()))
@@ -116,37 +121,19 @@ namespace QuickStore
                         if (disallow.Contains(objects[j].GetGroup().GetId()))
                             continue;
                     }
-                    else if (inventory.IsFull()) continue;
-
-                    //Work with and Bool Flag to make to logic better to read.
-                    bool allownamedstore = false;
-                    //check if Storing, depeding on ContainerName is exactly the ObjectName, is allowed.
-                    if ( allowStoreIfContainerNameisexact.Value && ial[i].GetComponent<WorldObjectText>()?.GetText().ToLower() == Readable.GetGroupName(objects[j].GetGroup()).ToLower()) allownamedstore = true;
-                    //check if Storing, depeding on ContainerName contains ObjectName, is allowed. If yes check if Container has the Name to proceed
-                    if ( allowStoreIfContainerNameContains.Value && ( ial[i].GetComponent<WorldObjectText>().GetText().ToLower().Contains($" {Readable.GetGroupName(objects[j].GetGroup()).ToLower()} ") || ial[i].GetComponent<WorldObjectText>().GetText().ToLower().StartsWith($"{Readable.GetGroupName(objects[j].GetGroup()).ToLower()} ") || ial[i].GetComponent<WorldObjectText>().GetText().ToLower().EndsWith($" {Readable.GetGroupName(objects[j].GetGroup()).ToLower()}")) )
+                    var itemName = Readable.GetGroupName(objects[j].GetGroup()).ToLower();
+                    if (
+                        (!storeIfContainerNameExact.Value || containerName != itemName) &&
+                        (!storeIfContainerNameContains.Value || !containerName.Contains(itemName)) &&
+                        (!storeIfAlreadyContains.Value || !inventory.GetInsideWorldObjects().Exists(o => o.GetGroup() == objects[j].GetGroup()))
+                        )
+                        continue;
+                    Dbgl($"Storing {objects[j].GetGroup()} in {ial[i].name}");
+                    if (inventory.AddItem(objects[j]))
                     {
-                            //Now check if a NameFlag is required
-                            if (requireNameFlagtoStore.Value)
-                            {
-                                //Check for the Name Flag. To make sure to not make a false positiv only accept free standing Flags. Include Start and End where a Space is missing.
-                                if ( ial[i].GetComponent<WorldObjectText>().GetText().ToLower().Contains($" {NameFlag.Value.ToLower()} ") || ial[i].GetComponent<WorldObjectText>().GetText().ToLower().StartsWith($"{NameFlag.Value.ToLower()} ") || ial[i].GetComponent<WorldObjectText>().GetText().ToLower().EndsWith($" {NameFlag.Value.ToLower()}")) allownamedstore = true;
-                            }
-                            else
-                            {
-                                allownamedstore = true;
-                            }
-                    }
-
-                    if (allownamedstore || ( allowstoreIfContainerAlreadyContainsSameItem.Value && inventory.GetInsideWorldObjects().Exists(o => o.GetGroup() == objects[j].GetGroup()) ) )
-                    {
-                        Dbgl($"Storing {objects[j].GetGroup()} in {ial[i].name}");
-                        if (inventory.AddItem(objects[j]))
-                        {
-                            informationsDisplayer.AddInformation(2f, Readable.GetGroupName(objects[j].GetGroup()), DataConfig.UiInformationsType.OutInventory, objects[j].GetGroup().GetImage());
-                            Managers.GetManager<PlayersManager>().GetActivePlayerController().GetPlayerBackpack().GetInventory().RemoveItem(objects[j]);
-                            if (inventory.IsFull())
-                                break;
-                        }
+                        informationsDisplayer.AddInformation(2f, Readable.GetGroupName(objects[j].GetGroup()), DataConfig.UiInformationsType.OutInventory, objects[j].GetGroup().GetImage());
+                        Managers.GetManager<PlayersManager>().GetActivePlayerController().GetPlayerBackpack().GetInventory().RemoveItem(objects[j]);
+                        objects.RemoveAt(j);
                     }
                 }
 
